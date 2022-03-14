31cd5454931fa345791c051f471551a183a18d77
levon <airtk@ya.ru>
2022-03-14 17:37:59 +0400

    первая версия игры 14.03.2022 день рождения через 2 дня сииииииии

--- jllbjk.py
+++ jllbjk.py
@@ -1,6 +1,9 @@
 import wrap
 
 wrap.world.create_world(1000,800)
+wrap.add_sprite_dir("kiberpank_file")
+
+kiberpank=wrap.sprite.add("kiberpank_id",500,500, "kiberpank")
 
 
 
@@ -11,10 +14,7 @@ wrap.world.create_world(1000,800)
 
 
 
-
-
-
-
+#добавить обычный спрайт
 
 
 import wrap_py
