203237fa56b39b77c9b1f6a7d72bde9cec6879b3
levon <airtk@ya.ru>
2022-03-19 15:16:06 +0400

    2 версия игры 14.03.2022 день рождения через 2 дня сииииииии

--- jllbjk.py
+++ jllbjk.py
@@ -2,16 +2,23 @@ import wrap
 
 wrap.world.create_world(1000,800)
 wrap.add_sprite_dir("kiberpank_file")
+wrap.world.set_back_image("fion/foi/fon.jpg")
 
 kiberpank=wrap.sprite.add("kiberpank_id",500,500, "kiberpank")
+dom=wrap.sprite.add("kiberpank_id",50,50, "dombabeegi")
 
 
 
+@wrap.on_key_always(wrap.K_w)
+def dvizhenie(keys,pos_x,pos_y):
+
+    if wrap.K_w in keys:
+        wrap.sprite.move_at_angle_dir(kiberpank,15)
 
 
-
-
-
+@wrap.always
+def fswefw (pos_y,pos_x):
+    wrap.sprite.set_angle_to_point(kiberpank,pos_x,pos_y)
 
 
 #добавить обычный спрайт
