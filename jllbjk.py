78da3a737ce373abb36523864ce11a9c154f7a66
levon <airtk@ya.ru>
2022-03-21 18:02:42 +0400

    версия 3 хоть както можно играть вэту к0каху

--- jllbjk.py
+++ jllbjk.py
@@ -5,7 +5,7 @@ wrap.add_sprite_dir("kiberpank_file")
 wrap.world.set_back_image("fion/foi/fon.jpg")
 
 kiberpank=wrap.sprite.add("kiberpank_id",500,500, "kiberpank")
-dom=wrap.sprite.add("kiberpank_id",50,50, "dombabeegi")
+dom=wrap.sprite.add("kiberpank_id",100,100, "dombabeegi")
 
 
 
@@ -14,15 +14,29 @@ def dvizhenie(keys,pos_x,pos_y):
 
     if wrap.K_w in keys:
         wrap.sprite.move_at_angle_dir(kiberpank,15)
+    sdvin_Eart()
 
 
-@wrap.always
+@wrap.on_mouse_move()
 def fswefw (pos_y,pos_x):
     wrap.sprite.set_angle_to_point(kiberpank,pos_x,pos_y)
 
 
-#добавить обычный спрайт
 
+
+def sdvin_Eart():
+    global kiberpank
+    print("0000")
+    x_kiberpanka_do_sdviga=wrap.sprite.get_x(kiberpank)
+    y_kiberpanka_do_sdviga=wrap.sprite.get_y(kiberpank)
+
+    wrap.sprite.move_to(kiberpank,500,500)
+    wrap.sprite.move(dom,500-x_kiberpanka_do_sdviga,500-y_kiberpanka_do_sdviga)
+
+
+
+
+#нужн0 что бы мир двигался во крук героя
 
 import wrap_py
 wrap_py.app.start()
