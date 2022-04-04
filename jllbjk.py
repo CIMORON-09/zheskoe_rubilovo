99f89390b0009838af413d7436e73a1eb42f437c
levon <airtk@ya.ru>
2022-04-04 18:03:09 +0400

    версия 5 хоть както можно играть вэту к0каху kurva

--- jllbjk.py
+++ jllbjk.py
@@ -1,3 +1,5 @@
+import random
+
 import wrap
 
 wrap.world.create_world(1000,800)
@@ -8,6 +10,7 @@ kiberpank=wrap.sprite.add("kiberpank_id",500,500, "kiberpank")
 dom=wrap.sprite.add("kiberpank_id",100,100, "dombabeegi")
 patron = wrap.sprite.add("kiberpank_id",500,500, "patron")
 wrap.sprite.hide(patron)
+limon = []
 @wrap.on_key_always(wrap.K_w)
 def dvizhenie(keys,pos_x,pos_y):
 
@@ -33,11 +36,21 @@ def sdvin_Eart():
     wrap.sprite.move_to(kiberpank,500,500)
     wrap.sprite.move(dom,500-x_kiberpanka_do_sdviga,500-y_kiberpanka_do_sdviga)
 
-@wrap.always(delay=1000)
+@wrap.always(delay=100)
 def delaem_polet(mouse_buttons):
-    print(mouse_buttons)
-    # if  wrap.BUTTON_LEFT==1   :
-    wrap.sprite.move(patron,0,0-7)
+    kol = 0
+    if  wrap.BUTTON_LEFT  in mouse_buttons  :
+        kol=1
+        l=mouse_buttons[0]
+
+        limon.append(l)
+
+
+        lol=limon[0]
+        pol=limon[-1]
+        poi=random.randint(lol,pol)
+        if poi==1 :
+            wrap.sprite.move(patron,0,0-10)
 #нужн0 что бы мир двигался во крук героя
 
 
@@ -50,7 +63,6 @@ def delaem_strelbu():
         kiberpnk_gradus = wrap.sprite.get_angle(kiberpank)
         wrap.sprite.set_angle(patron, kiberpnk_gradus)
         wrap.sprite.show(patron)
-    wrap.sprite.move_at_angle_dir(patron,-7)
 
 import wrap_py
 wrap_py.app.start()
